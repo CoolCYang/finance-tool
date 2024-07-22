275aec2570f2533bba8561c4ea47ce7a9a2c6acb
JiaSen <jiasen@datatell.com.cn>
2024-07-22 17:04:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-config-provider namespace="ep">
+    <div class="flex p-2 h-full box-border">
+      <RouterView />
+    </div>
+  </el-config-provider>
+</template>
+
+<style>
+#app {
+  height: 100%;
+  overflow: hidden;
+  background-color: #f5f5f5;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter } from 'vue-router'
+import { createWebHistory } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'login',
+    component: () => import('~/views/index.vue')
+  }
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+  strict: true,
+  scrollBehavior(to, from, savedPosition) {
+    return new Promise((resolve) => {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        if (from.meta.saveSrollTop) {
+          const top =
+            document.documentElement.scrollTop || document.body.scrollTop
+          resolve({ left: 0, top })
+        }
+      }
+    })
+  }
+})
+
+router.beforeEach((to, from, next) => {
+  next()
+})
+
+export default router
